d8096359ce857df8deb5f7404ce83b12406a461f
Kashish <kashishchugh2001@gmail.com>
2020-10-30 03:44:55 +0530

    add Routes and Login UI

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,90 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
-function App() {
-  return <div className="App">App</div>;
+import { fetchPosts } from '../actions/posts';
+import { Home, Navbar, Page404, Login } from './index';
+import PropTypes from 'prop-types';
+import { BrowserRouter as Router, Link, Route, Switch } from 'react-router-dom';
+// import React, { Component } from 'react'
+
+// const Home = () => <div>Home</div>;
+// const Lagin = () => <div>Lagin</div>;
+class App extends React.Component {
+  componentDidMount() {
+    this.props.dispatch(fetchPosts());
+  }
+
+  render() {
+    // console.log('Props:', this.props);
+    const { posts } = this.props;
+    return (
+      <Router>
+        <div>
+          <Navbar />
+
+          {/* <PostsList posts={posts} /> */}
+          <Switch>
+            <Route
+              exact
+              path="/"
+              render={(props) => {
+                return <Home {...props} posts={posts} />;
+              }}
+            />
+            <Route path="/login" component={Login} />
+            <Route component={Page404} />
+          </Switch>
+        </div>
+      </Router>
+
+      // <div className="posts-list">
+      //   {posts.map((post) => {
+      //     <div className="post-wrapper" key={post._id}>
+      //       <div className="post-header">
+      //         <div className="post-avatar">
+      //           <img
+      //             src="https://www.flaticon.com/svg/static/icons/svg/560/560216.svg"
+      //             alt="user-image"
+      //           ></img>
+      //         </div>
+      //         <div>
+      //           <span className="post-author">{post.user.name}</span>
+      //           <span className="post-time">{post.createdAt}</span>
+      //         </div>
+      //       </div>
+      //       <div className="post-content">{post.content}</div>
+      //       <div className="posts-actions">
+      //         <div className="post-like">
+      //           <img
+      //             src="https://www.flaticon.com/svg/static/icons/svg/1029/1029183.svg"
+      //             alt="likes-icon"
+      //           />
+      //           <span>{post.likes.length}</span>
+      //         </div>
+
+      //         <div className="post-comments-icon">
+      //           <img
+      //             src="https://www.flaticon.com/svg/static/icons/svg/1380/1380338.svg"
+      //             alt="comment-icon"
+      //           />
+      //           <span>{post.comments.length}</span>
+      //         </div>
+      //         <div className="post-comment-box">
+      //           <input placeholder="Type a Comment" />
+      //         </div>
+      //       </div>
+      //     </div>;
+      //   })}
+      // </div>
+    );
+  }
 }
 
-export default App;
+function mapStateToProps(state) {
+  return { posts: state.posts };
+}
+
+App.propTypes = {
+  posts: PropTypes.array.isRequired,
+};
+export default connect(mapStateToProps)(App);
